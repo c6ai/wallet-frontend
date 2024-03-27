0300a18ae70753c92500a0f450f97c6677d9e9d0
Emil Lundberg <emil@yubico.com>
2024-03-27 16:16:36 +0100

    Import LocalStorageKeystore in test

--- src/services/LocalStorageKeystore.test.ts
+++ src/services/LocalStorageKeystore.test.ts
@@ -1,3 +1,8 @@
+import { useLocalStorageKeystore } from "./LocalStorageKeystore";
+
+
 it('runs the test', () => {
+	const keystore = useLocalStorageKeystore();
+	console.log(keystore);
 	expect(false).toBe(true);
 });
